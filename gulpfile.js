f351d1fbc749d5745bab8262035d7d85116d24c7
educom <educom@educomui-iMac-2.local>
2016-06-18 16:36:22 +0900

    final

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var jsfiles = ["./src/js/config.js",
 gulp.task('concat:js', function() {
   return gulp.src(jsfiles)
     .pipe(concat('app.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/'));
 });
 
@@ -31,14 +32,15 @@ gulp.task('copy:lib', function() {
     .pipe(gulp.dest('dist/lib'));
 });
 
-gulp.task('minify:js', function() {
-  return gulp.src('src/dist/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
-});
+// gulp.task('minify:js', function() {
+//   return gulp.src('src/dist/app.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist'));
+// });
 
 gulp.task('minify:css', function() {
   return gulp.src('src/css/*.css')
+    .pipe(cleanCSS())
     .pipe(gulp.dest('dist'));
 });
 
